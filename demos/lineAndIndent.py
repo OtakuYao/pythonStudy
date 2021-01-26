d47c6974f06356329b07d4c7a655c7d233c3b481
明のPC <yym_xiaoming@foxmail.com>
2021-01-26 18:31:43 +0800

    缩进练习

---
+++ demos/lineAndIndent.py
@@ -0,0 +1,15 @@
+# 行和缩进   line and indent
+
+if True:
+    print("true")
+else:
+    print("false")
+
+
+'''
+if True:
+     print("true") ----如果缩进不一直会报错：Unindent does not match any outer indentation level
+    print("true")
+else:
+    print("false")
+'''
