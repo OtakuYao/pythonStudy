6f31da3e8d8ca322240e30a5e9ba82083039637d
明のPC <yym_xiaoming@foxmail.com>
2021-01-27 17:49:25 +0800

    修改

--- demos/lineAndIndent.py
+++ demos/lineAndIndent.py
@@ -1,15 +1,9 @@
 # 行和缩进   line and indent
 
 if True:
+    # print("true") ----如果缩进不一致会报错：Unindent does not match any outer indentation level
     print("true")
 else:
     print("false")
 
 
-'''
-if True:
-     print("true") ----如果缩进不一直会报错：Unindent does not match any outer indentation level
-    print("true")
-else:
-    print("false")
-'''
